7874cef98c9968b24c1f0bb8881cba81c52c01ad
Fenik01110011 <marcinbialecki1997@gmail.com>
2023-01-13 13:05:08 +0100

    Added support for obstacle sensor. The robot stops but does not avoid obstacles.

--- src/main.cpp
+++ src/main.cpp
@@ -8,14 +8,14 @@
 #define PIN_PWM_L 5
 
 // PWN controler
-#define PIN_CH1_CONTROLER 6 //prawy joystick, sterowanie na boki
+//#define PIN_CH1_CONTROLER 6 //prawy czujnik,
 #define PIN_CH2_CONTROLER 7 //prawy joystick, predkosc prawych kol
 #define PIN_CH3_CONTROLER 8 //lewy joystick, predkosc lewych kol
-#define PIN_CH4_CONTROLER 9 //lewy joystick, sterowanie na boki
+//#define PIN_CH4_CONTROLER 9 //lewy czujnik
 #define PIN_CH5_CONTROLER 10 //obracanie sie lazika
-#define PIN_CH6_CONTROLER 11 //szybkosc autoatycznego obracania sie lazika odnoscie CH5 
+//#define PIN_CH6_CONTROLER 11 //szybkosc autoatycznego obracania sie lazika odnoscie CH5 
 #define PIN_CH7_CONTROLER 12 
-#define PIN_CH8_CONTROLER 13
+//#define PIN_CH8_CONTROLER 13
 
 // DIR is connected to pin 2 and 4.
 #define PIN_DIR_R 2
@@ -25,26 +25,42 @@
 #define R_PIN A0
 #define L_PIN A1
 
-int defaultLightIntensity = 0;
-int rPinLightIntensity = 0;
-int lPinLightIntensity = 0;
-
 #define BASIC_CONTROLER_INPUT 1500
 #define MAX_CONTROLER_INPUT 1900
 #define MIN_CONTROLER_INPUT 1100
+
+//Sensor connection
+#define R_T_SENSOR 9
+#define R_E_SENSOR 6
+#define L_T_SENSOR 13
+#define L_E_SENSOR 11
+
+int defaultLightIntensity = 0;
+int rPinLightIntensity = 0;
+int lPinLightIntensity = 0;
 
 int speedR = 0;
 int speedL = 0;
 
 int ch5Signal;
-int ch6Signal;
+//int ch6Signal;
 int ch7Signal;
 
-int autoModStatus = 0;
+bool autoModStatus = false;
+
+//Ultrasonic distance sensor (0 - right sensor, 1 - left sensor)
+int echo[2];
+int trig[2];
+int distanceReading[2];
+
+long h_time, h_distance;
 
 /*******************************************************************************
  * FUNCTIONS                                                                    *
  *******************************************************************************/
+
+void hcsr_begin(int index, int echoPin, int trigPin);
+float hcsr_getDistance(int index);
 
 // The setup routine runs once when you press reset.
 void setup() {
@@ -55,14 +71,14 @@ void setup() {
   pinMode(PIN_DIR_R, OUTPUT);
   pinMode(PIN_DIR_L, OUTPUT);
 
-  pinMode(PIN_CH1_CONTROLER, INPUT);
+  //pinMode(PIN_CH1_CONTROLER, INPUT);
   pinMode(PIN_CH2_CONTROLER, INPUT);
   pinMode(PIN_CH3_CONTROLER, INPUT);
-  pinMode(PIN_CH4_CONTROLER, INPUT);
+  //pinMode(PIN_CH4_CONTROLER, INPUT);
   pinMode(PIN_CH5_CONTROLER, INPUT);
-  pinMode(PIN_CH6_CONTROLER, INPUT);
+  //pinMode(PIN_CH6_CONTROLER, INPUT);
   pinMode(PIN_CH7_CONTROLER, INPUT);
-  pinMode(PIN_CH8_CONTROLER, INPUT);
+  //pinMode(PIN_CH8_CONTROLER, INPUT);
 
   digitalWrite(PIN_DIR_R, HIGH);
   digitalWrite(PIN_DIR_L, HIGH);
@@ -70,6 +86,12 @@ void setup() {
   pinMode(R_PIN, INPUT);
   pinMode(L_PIN, INPUT);
   defaultLightIntensity = (analogRead(R_PIN) + analogRead(L_PIN))/ 2;
+
+  //Sensor activate
+  hcsr_begin (0, R_E_SENSOR, R_T_SENSOR);
+  hcsr_begin (1,L_E_SENSOR, L_T_SENSOR);
+  
+  //Sensor distance return
 }
 
 // The loop routine runs over and over again forever.
@@ -78,14 +100,14 @@ void loop() {
     speedR = pulseIn(PIN_CH2_CONTROLER, HIGH);
     speedL = pulseIn(PIN_CH3_CONTROLER, HIGH);
     ch5Signal = pulseIn(PIN_CH5_CONTROLER, HIGH);
-    ch6Signal = pulseIn(PIN_CH6_CONTROLER, HIGH);
+    //ch6Signal = pulseIn(PIN_CH6_CONTROLER, HIGH);
     ch7Signal = pulseIn(PIN_CH7_CONTROLER, HIGH);
 
     //Ustawienia automatycznego dzialania lazika
     if(ch7Signal < 1300)
-      autoModStatus = 1;
+      autoModStatus = true;
     else
-      autoModStatus = 0;
+      autoModStatus = false;
 
     //zatrzymanie lazika jesli kontroler jest wylaczony
     if(speedR < 930 || speedR > 2070 || speedL < 930 || speedL > 2070 || ((ch7Signal > 1300 && ch7Signal < 1700) || ch7Signal < 800))
@@ -94,7 +116,7 @@ void loop() {
       analogWrite(PIN_PWM_L, 0);
     }
     else {
-      if(autoModStatus == 0)
+      if(autoModStatus == false)
       {
         if(speedR >= (BASIC_CONTROLER_INPUT - 50) && speedR <= (BASIC_CONTROLER_INPUT + 50) && speedL >= (BASIC_CONTROLER_INPUT - 50) && speedL <= (BASIC_CONTROLER_INPUT + 50)){
           rPinLightIntensity = analogRead(R_PIN);
@@ -134,23 +156,26 @@ void loop() {
               digitalWrite(PIN_DIR_R, HIGH);
               digitalWrite(PIN_DIR_L, LOW);
             }
-            if(ch5Signal > 1700){
+            else if(ch5Signal > 1700){
               digitalWrite(PIN_DIR_R, LOW);
               digitalWrite(PIN_DIR_L, HIGH);
             }
+
+            analogWrite(PIN_PWM_R, 255);
+            analogWrite(PIN_PWM_L, 255);
             
-            if(ch6Signal >= MAX_CONTROLER_INPUT){
-              analogWrite(PIN_PWM_R, 255);
-              analogWrite(PIN_PWM_L, 255);
-            }
-            else if(ch6Signal <= MIN_CONTROLER_INPUT){
-              analogWrite(PIN_PWM_R, 0);
-              analogWrite(PIN_PWM_L, 0);
-            }
-            else{
-              analogWrite(PIN_PWM_R, abs((ch6Signal - 1000)/4));
-              analogWrite(PIN_PWM_L, abs((ch6Signal - 1000)/4));
-            }
+            // if(ch6Signal >= MAX_CONTROLER_INPUT){
+            //   analogWrite(PIN_PWM_R, 255);
+            //   analogWrite(PIN_PWM_L, 255);
+            // }
+            // else if(ch6Signal <= MIN_CONTROLER_INPUT){
+            //   analogWrite(PIN_PWM_R, 0);
+            //   analogWrite(PIN_PWM_L, 0);
+            // }
+            // else{
+            //   analogWrite(PIN_PWM_R, abs((ch6Signal - 1000)/4));
+            //   analogWrite(PIN_PWM_L, abs((ch6Signal - 1000)/4));
+            // }
           }
         }
         else{
@@ -193,7 +218,72 @@ void loop() {
           }
         }
       }
+      else if(autoModStatus == true)
+      {
+        int speed[2];
+        digitalWrite(PIN_DIR_R, HIGH);
+        digitalWrite(PIN_DIR_L, HIGH);
+
+        for(int i = 0; i < 2; i++)
+        {
+          distanceReading[i] = hcsr_getDistance(i);
+          Serial.print(distanceReading[i]);
+          Serial.print("\t");
+
+          if(distanceReading[i] >= 255)
+            speed[i] = 255;
+          else if(distanceReading[i] < 20)
+          {
+            for(int j = 0; j < 2; j ++)
+              speed[j] = 0;
+            break;
+          }
+          else if(distanceReading[i] < 40)
+            speed[i] = 0;
+          else
+            speed[i] = 255 - (255 - distanceReading[i]);
+          
+          delay(30);
+        }
+        Serial.print("\n");
+
+        analogWrite(PIN_PWM_R, speed[1]);
+        analogWrite(PIN_PWM_L, speed[0]);
+      }
     }
 
     delay(10);
+
+    // if(!hcsr_getDistance(0)) Serial.print("ERROR");
+    // else Serial.print(hcsr_getDistance(0));
+
+    // Serial.print("\t");
+    // delay(30);
+    // if(!hcsr_getDistance(1)) Serial.print("ERROR");
+    // else Serial.print(hcsr_getDistance(1));
+    // Serial.print("\n");
+
+}
+
+void hcsr_begin(int index, int echoPin, int trigPin)
+  {
+    echo [index] = echoPin;
+    trig [index] = trigPin;
+
+    pinMode(echo[index], INPUT);
+    pinMode(trig[index], OUTPUT);
+
+  }
+
+float hcsr_getDistance(int index)
+{
+  digitalWrite(trig[index], LOW);
+  delayMicroseconds(2);
+  digitalWrite(trig[index], HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trig[index], LOW);
+  
+  h_time = pulseIn(echo[index], HIGH);
+  if(h_time < 0 || h_time >= 23200) return 0;
+  return h_time / 58.00;
 }
